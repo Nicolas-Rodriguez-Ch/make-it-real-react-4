269a2ae449133b059e00359e637fe2187b3f80fa
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-02-15 10:36:27 -0500

    creates the repo and does the exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [number, setNumber] = useState(0);
+const [temperature, setTemperature] = useState('celcius');
+const [display_temperature, setDisplay_temperature] = useState();
+
+const handleSumbit = (event)=> {
+  event.preventDefault();
+  if (temperature === 'celcius') {
+    setDisplay_temperature((9/5) * number + 32);
+  } else {
+    setDisplay_temperature((number - 32) * 5/9);
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSumbit} className='temperature__form'>
+        <input 
+          type='number' 
+          className='temperature__box'
+          value={number}
+          onChange={(event) => setNumber(event.target.value)}
+        > 
+        </input>
+        <select 
+          className='temperature__list'
+          value={temperature}
+          onChange={event => setTemperature(event.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="celcius">Celcius</option>
+          <option value="fahrenheit">Farenheit</option>
+        </select>
+        <button className='convert__button'>Convert</button>
+        <section className='temperature__conversion__result'>
+          <p>Result</p>
+          <span>{display_temperature}° {temperature !== 'celcius' ? 'C':'F'}</span>
+        </section>
+      </form>
     </div>
   );
 }
